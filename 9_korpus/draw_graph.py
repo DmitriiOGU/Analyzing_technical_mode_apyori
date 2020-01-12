d1e4c8918b1dfa02e7148b4ea4031c809bfe6743
Dmitrii_Ogurtsov <dimogu90@ya.ru>
2020-01-12 17:04:09 +0700

    Draw1

--- 9_korpus/draw_graph.py
+++ 9_korpus/draw_graph.py
@@ -6,8 +6,80 @@ Created on Thu Jan  9 16:37:34 2020
 """
 
 from graphviz import Digraph
-dot = Digraph(comment='The Round Table', format='png')
-for i in range(20):
-            dot.edge(str(i),str(i-1))
-print(dot.source)  
-dot.render('round-table', view=True) 
+import pandas as pd
+def plotting_graph(df_name,all_data,node_count=3):
+    out1=all_data.values.tolist()
+    transactions=[]
+    for i in out1:
+        transactions.append([x for x in i if str(x) != 'nan'])
+    df=pd.read_pickle(df_name)
+    key_value=df.items_add.value_counts().index[0][0]
+    list1=[]
+    for i in df.index:
+        if df.items_add[i][0]!=key_value:
+            list1.append(i)
+    df=df.drop(list1)
+    df=df.reset_index(drop=True)
+    df=df.sort_values(by=['support'],ascending=False)
+    list_items=[]
+    list_supp=[]
+    
+    for j in range(2,max(df.len_of_trans)+1):
+        list1=[]
+        k=0
+        for i in df.index:
+            if df.len_of_trans[i]==j:
+                list1.append(i)
+                if k<node_count:
+                    list_items.append(df.items_base[i])
+                    list_supp.append(df.support[i])
+                    k+=1
+        df=df.drop(list1)
+        df=df.reset_index(drop=True)
+    list_lifts = create_general_lifts(list_items,key_value,transactions)
+    print(list_lifts)
+    #Построение графа
+    dot = Digraph(comment='The Round Table', format='png')
+    for i in range(len(list_items)):
+        len_items=len(list_items[i])
+        counter=0
+        upper_level=['None']
+        supp=float('{:.3f}'.format(list_supp[i]*100))
+        lift=float('{:.3f}'.format(list_lifts[i]))
+        if len_items==1:
+            dot.edge(key_value+' support = 100%','['+key_value+", '"+list_items[i][0]+"']",'support = '+str(supp)+'%, lift = '+str(lift)) 
+        else:
+            for j in range(len(list_items)):
+                if ((len_items>len(list_items[j]))&(set(list_items[j]).issubset(list_items[i]))&(counter<len(list_items[j]))):
+                    counter=len(list_items[j])
+                    upper_level=(list_items[j])
+            dot.edge('['+key_value+', '+str(upper_level)[1:-1]+']','['+key_value+', '+str(list_items[i])[1:-1]+']','support = '+str(supp)+'%, lift = '+str(lift))         
+    print(dot.source)  
+    dot.render(df_name[:-4], view=False) 
+    
+def create_general_lifts(list_items,key_value,transactions):
+    list_lifts=[]
+    for i in list_items:
+        key_list=[]
+        key_list.append(key_value)
+        full_transaction=i.copy()
+        full_transaction.append(key_value)
+        x_y=0
+        x=0
+        y=0
+        for j in transactions:
+            if set(full_transaction).issubset(j):
+                x_y+=1
+            if set(i).issubset(j):
+                x+=1
+            if set(key_list).issubset(j):
+                y+=1
+        x_y/=len(transactions)
+        y/=len(transactions)
+        x/=len(transactions)
+        list_lifts.append((x_y)/(x*y))    
+    return list_lifts
+
+df_name='./base_not_base/Base_AE_0.pkl'
+#all_data=pd.read_excel('9_korpus_data_with_konus.xlsx',index_col=0)
+plotting_graph(df_name,all_data)
